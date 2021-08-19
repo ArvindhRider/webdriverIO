e623ea3a3b36931b5ed79b6d646851b845bc06d4
Arvindh <arvindh.official05@gmail.com>
2021-08-19 19:07:20 +0530

    back to learning

--- test/test_suite/uiControls.js
+++ test/test_suite/uiControls.js
@@ -1,6 +1,7 @@
 let chaiExpect = require('chai').expect; //Note this !!!
 
 describe('My Login application', () => {
+
   it('UI Validations test', () => {
     browser.url("https://rahulshettyacademy.com/loginpagePractise/#");
 
@@ -44,6 +45,7 @@ describe('My Login application', () => {
     browser.pause(1000);
 
     //Assertion from jasmine 
+    //getValue() --> method
     expect(dropDown.getValue()).toBe("teach"); // will get the value of "dropdown"
 
     // //To get a better deeper understanding or to assert better we use the chai library from npm
@@ -51,6 +53,57 @@ describe('My Login application', () => {
     chaiExpect(dropDown.getValue()).to.contain("teach");
     chaiExpect(dropDown.getValue()).to.equal("teach");
     // //Negative scenario
-    chaiExpect(dropDown.getValue()).to.equal("teac");   
+   // chaiExpect(dropDown.getValue()).to.equal("teac");   
   });
+
+  it('UI Validations Auto suggestive drop downs', () => {
+    //Auto suggestive dropdowns
+    browser.url("https://rahulshettyacademy.com/AutomationPractice/");
+    let dropDownBox = $('#autocomplete');
+    dropDownBox.setValue('Ind');
+    browser.pause(2000); // for Rider to see the options
+
+    //Logic :- collecting all the options from the dom and if our value is available there we check and select
+
+    let items = $$("//li[@class='ui-menu-item']/div");
+    //Method 1 
+    // for(let suggestion of items) {
+    //   let options = suggestion.getText();
+    //   console.log(options);
+    //   if(options === "India") {
+    //     suggestion.click();
+    //   }
+    // }
+    // browser.pause(2500);
+
+    //Method 2
+   // dropDownBox.clearValue();
+    dropDownBox.setValue('Ind');
+    browser.pause(2000); // for Rider to see the options
+    const myCountry = items.filter(suggestion => suggestion.getText() === "British Indian Ocean Territory");
+    //Filter returns elements in an array , as per our condition there is onlty one indonesia and tat will sit in the 0th position
+    myCountry[0].click();
+    browser.pause(2500);
+  });
+
+  it('UI Validation checkboxes', () => {
+    let checkBox = $('#checkBoxOption1');
+    checkBox.click();
+    expect(checkBox.isSelected()).toBe(true);
+    browser.saveScreenshot("C:\\Users\\ARVINDH\\Desktop\\desktop junks\\trail.png");
+  });
+
+  it('Mouse hover practice', () => {
+    let mouseHoverElement = $("#mousehover");
+    mouseHoverElement.scrollIntoView();
+    mouseHoverElement.moveTo();
+    browser.pause(2500);
+    let mouseHoverOption = $("a[href='#top']") 
+    mouseHoverOption.click();
+    browser.pause(2500);
+  });
+
+
+
+ 
 });
